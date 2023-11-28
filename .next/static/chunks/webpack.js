879420b2353495eb8b468bf9dcb078d88e3b99cb
Kris <56274379+KrisEgidio@users.noreply.github.com>
2023-11-28 00:14:36 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1bb4a6c96a1331a8"; }
+/******/ 		__webpack_require__.h = function() { return "54540328e7ff869d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Não se preocupe! Independe do caminho que escolher, eu sei que vai conseguir ❤️</h1>
+function Home() {
+  return (
+    <h1>
+      Não se preocupe! Independe do caminho que escolher, eu sei que vai
+      conseguir ❤️
+    </h1>
+  );
 }
 
 export default Home;
